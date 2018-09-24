f811b5d066c9355ee76c3d001782bcc435e81d1c
Boris Radchenko <radchenko.boris@gmail.com>
2018-09-24 09:13:27 +0300

    Initial commit to remote origin

---
+++ Chipldapp/Controllers/Main screen/MainScreenController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chipldapp
+//
+//  Created by Mr.Б on 24/09/2018.
+//  Copyright © 2018 KexitSoft. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
